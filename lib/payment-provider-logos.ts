31340e720280879f97cd995318a4ca48c97c1d31
anamaherbert1-glitch <anamaherbert1@gmail.com>
2026-09-16 13:54:38 +0000

    Add official payment provider badge URLs

---
+++ lib/payment-provider-logos.ts
@@ -0,0 +1,11 @@
+export const OFFICIAL_PAYMENT_LOGOS: Record<string, string> = {
+  cinetpay: 'https://docs.cinetpay.com/images/logo-new.png',
+  flutterwave: 'https://flutterwave.com/favicon.ico',
+  paydunya: 'https://paydunya.com/favicon.ico',
+  wave: 'https://www.wave.com/favicon.ico',
+  saspay: 'https://saspay.me/favicon.ico',
+  ligdicash: 'https://www.ligdicash.com/favicon.ico',
+  hub2: 'https://www.hub2.io/favicon.ico',
+  fedapay: 'https://www.fedapay.com/favicon.ico',
+  campay: 'https://www.campay.net/favicon.ico',
+}
